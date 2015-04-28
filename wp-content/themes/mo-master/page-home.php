93ddc9b5793b07d7f8c6f3cd80c086ca933cc421
muminur1 <muminur1@gmail.com>
2015-04-28 10:24:45 -0500

    update

--- wp-content/themes/mo-master/page-home.php
+++ wp-content/themes/mo-master/page-home.php
@@ -120,15 +120,10 @@
 				<div class="col-sm-12 col-md-6 col-lg-6">
 					<h3>Find Me</h3>
 					<h1>
-					<i class="fa fa-facebook-square"></i>
-					<i class="fa fa-github-square"></i>
-					<i class="fa fa-google-plus-square"></i>
-					<i class="fa fa-linkedin-square"></i>
-					<i class="fa fa-steam-square"></i>
-					<i class="fa fa-bitbucket-square"></i>
-					<i class="fa fa-skype"></i>
-					<i class="fa fa-apple"></i>
-					<i class="fa fa-twitter-square"></i>
+					<a target="_blank" href="https://www.facebook.com/muminur.rahman.9"><i class="fa fa-facebook-square"></i></a>
+					<a target="_blank" href="https://plus.google.com/117217477889898847302/posts"><i class="fa fa-google-plus-square"></i></a>
+					<a target="_blank" href="https://www.linkedin.com/pub/muminur-rahman/67/326/589"><i class="fa fa-linkedin-square"></i></a>
+					<a target="_blank" href="https://github.com/muminur1"><i class="fa fa-github-square"></i></a>
 					</h1>
 				</div>
 				<div class="col-sm-12 col-md-6 col-lg-6">
